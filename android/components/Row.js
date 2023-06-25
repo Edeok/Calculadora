f8455357cb402f72e875a60fb18ba448d6c4d12a
Edeok <carosartini.cs@gmail.com>
2023-06-25 00:18:08 -0300

    Calculadora

---
+++ android/components/Row.js
@@ -0,0 +1,13 @@
+import { StyleSheet, View } from "react-native/types";
+
+const Row = ({ children })=>{
+    return <View style={styles.container}>{children}</View>
+};
+
+//create styles of Row
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+    },
+});
+export default Row;
